208abe359e959988108a14d5db01d0f92f825a4f
popolopo <maafidavid@gmail.com>
2023-07-12 16:29:31 +0200

    feat: add main.py

--- main.py
+++ main.py
@@ -5,49 +5,49 @@ from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingP
 from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
 
-# STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data Ingestion stage"
 
-# try:
-#     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
-#     data_ingestion = DataIngestionTrainingPipeline()
-#     data_ingestion.main()
-#     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME = "Data Validation stage"
+STAGE_NAME = "Data Validation stage"
 
-# try:
-#     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
-#     data_validation = DataValidationTrainingPipeline()
-#     data_validation.main()
-#     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME = "Data Transformation stage"
+STAGE_NAME = "Data Transformation stage"
 
-# try:
-#     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
-#     data_validation = DataTransformationTrainingPipeline()
-#     data_validation.main()
-#     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_validation = DataTransformationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME = "Model Trainer stage"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_trainer = ModelTrainerTrainingPipeline()
-#    model_trainer.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
